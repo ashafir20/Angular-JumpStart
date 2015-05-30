4d723c322cf159b9075343d2a2da0595769a2c2f
ashafir <ashafir20@gmail.com>
2015-05-30 23:44:09 +0300

    using a controller

---
+++ app/controllers/customersController.js
@@ -0,0 +1,15 @@
+function CustomersController($scope){
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+
+    $scope.customers = [
+           {joined: '2000-12-02', name: 'John', city: 'Chicago', orderTotal: 9.99956},
+           {joined: '2000-12-02', name: 'John', city: 'Chicago', orderTotal: 12.99956},
+           {joined: '2000-12-02', name: 'John', city: 'Chicago', orderTotal: 3.99956},
+           {joined: '2012-12-02', name: 'Aviv', city: 'Tel Aviv', orderTotal: 15.99956}
+       ];
+    $scope.doSort = function(propName) {
+        $scope.sortBy = propName;
+        $scope.reverse = !$scope.reverse;
+    }
+}
